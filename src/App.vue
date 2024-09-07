cc44b6ab0f5533c53ca3e3dd268f60c696b4917a
trungtan <trungtan14@gmail.com>
2024-09-07 15:51:41 +0700

    done

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,8 @@
   <div id="app">
     <div
       class="absolute inset-0 -z-10 h-full w-full bg-[#4ec5c1] bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]"
-    >
-      <TaskTodo></TaskTodo>
-    </div>
+    ></div>
+    <TaskTodo />
   </div>
 </template>
 
